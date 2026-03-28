c37a425a51dc7ecaabd89cf65fb59ae3d2e93c08
Prakan Suma <prakan.suma@gmail.com>
2026-03-28 23:27:23 +0700

    set up to prod

--- verify.sql
+++
@@ -1,6 +0,0 @@
-SELECT count(*) FROM "user";
-SELECT count(*) FROM classroom;
-SELECT count(*) FROM member;
-SELECT count(*) FROM assignment;
-SELECT count(*) FROM playground;
-SELECT count(*) FROM submission;
